5b009ffb07a55365577ec36f8182c608c049cb29
ozymok1604 <ozimok1604ua@gmail.com>
2022-02-11 02:37:36 +0200

    Max ***

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-console.log('Hello,world!!!');
-alert("Hello")
+namee = prompt("Введіть ім'я:");
+function formatName(namee){
+    index = namee.indexOf(' '); 
+
+
+    let string = namee[0].toUpperCase() + namee.slice(1,index) +' ' + namee[index+1].toUpperCase() + namee.slice(index+2);
+    console.log(string);
+
+
+}
+formatName("namee");
